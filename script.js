3024fd391eb2c1e9dece0fd14b4188ba4d0b4671
@DaljyotG <daljyotgrewal@hotmail.com>
2024-08-14 11:59:07 -0400

    Fix JS Json

--- script.js
+++ script.js
@@ -95,11 +95,11 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     .then(response => response.json()) // Parse the JSON response
     .then(data => {
         // Check if the status is success or failure
-        if (data.message && data.message === 'Data stored successfully and email sent') {
+        if (data.body.message && data.body.message === 'Data stored successfully and email sent') {
             document.getElementById('responseBanner').innerText = 'Thank you for your message! We will get back to you soon.';
             document.getElementById('responseBanner').style.color = 'green';
         } else {
-            throw new Error(data.message || 'An error occurred');
+            throw new Error(data.body.message || 'An error occurred');
         }
         console.log('Success:', data);
     })
